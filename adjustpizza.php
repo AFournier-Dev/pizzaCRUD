0fb5fbaeffb1666af169b2b9b1bdcc3a71abc90f
AFournier-Dev <alexandre.fournier@yahoo.fr>
2020-11-09 16:47:09 +0100

    adjust OK TBD deletepizza WIP

--- adjustpizza.php
+++ adjustpizza.php
@@ -1,20 +1,24 @@
-<?php 
+<?php
 require_once 'header.php';
 require_once 'db.php';
-require_once 'functions.php'; 
+require_once 'functions.php';
 logged_only();
-$id= intval($_GET['id']);
+
+if (!empty($_POST)) {
+    $req = $pdo->prepare("UPDATE pizza_list SET pizza_name = ?, ingredient = ? WHERE id= ? ");
+    $req->execute([$_POST['pizza_name'], $_POST['ingredient'], $_POST['data']]);
+}
+
+$id = intval($_GET['id']);
 $req = $pdo->prepare("SELECT * FROM pizza_list WHERE id=?");
 $req->execute([$id]);
-$result= $req->fetch();
+$result = $req->fetch();
 
-if($result->pizza_inventor != $_SESSION['auth']->id){
-echo('Vous ne pouvez pas modifier cette pizza');
-
-}
-else{
-    ?>
-   <form action="" method="POST">
+if ($result->pizza_inventor != $_SESSION['auth']->id) {
+    echo ('Vous ne pouvez pas modifier cette pizza');
+} else {
+?>
+    <form action="" method="POST">
         <div class="form-group">
             <label for="">Nom de votre Pizza</label>
             <input type="text" name="pizza_name" class="form-control" placeholder="Uniquement des caractères alphabétiques non accentués et espaces" value="<?php echo $result->pizza_name ?>" required>
@@ -28,21 +32,9 @@ else{
         <button type="submit" class="btn btn-primary" name="updatepizza">Modifier votre pizza</button>
     </form>
 
-    <?php
-    var_dump($result->id);
-    if($result->pizza_inventor != intval($result->id)) {
-        echo('Vous ne pouvez pas modifier cette pizza');
-    }
-    else{
-$req = $pdo->prepare("UPDATE pizza_list SET pizza_name = ?, ingredient = ?, WHERE pizza_inventor= $_POST'data' ");
-$req->execute([$_POST['pizza_name'], $_POST['ingredient']]);
-    }
+<?php
     
 }
-
-
-
-var_dump($result);
 ?>
 
 <?php require_once 'footer.php'; ?>
